4553359be860f92f9c720750d66300fa6b38a406
Gervin Luide <gervin.luide@khk.ee>
2022-10-19 14:13:30 +0300

    addBookToLocalStorage funktsionaalsuse lisamine

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ submit.addEventListener("click", function (){
         a.setAttribute("href", "#")
         td_x.appendChild(a)
         booklistrow.appendChild(td_x);
+        addBookLS(titlein.value, authorin.value, isbnin.value)
 
         list.appendChild(booklistrow);
 
@@ -41,8 +42,46 @@ function deleteTask(e){
     if(e.target.textContent == 'X'){
         if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.parentElement.remove()
+            let bookIsbn = e.target.parentElement.previousElementSibling
+            let bookAuthor = bookIsbn.previousElementSibling
+            let bookTitle = bookAuthor.previousElementSibling
+            let book = [bookTitle.textContent, bookAuthor.textContent, bookIsbn.textContent]
+
+            deleteBookLS(book)
         }
     }
 }
+
+
+function addBookLS(title, author, isbn) {
+    const andmetelist = [title, author, isbn]
+
+    let books
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.push(andmetelist)
+    localStorage.setItem('books', JSON.stringify(books))
+}
+
+function deleteBookLS(book) {
+    const andmetelist = [title, author, isbn]
+    let books
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.forEach((bookLS, bookIndex) => {
+                if(JSON.stringify(bookLS) === JSON.stringify(book)){
+                    books.splice(bookIndex, 1)
+                }
+            })
+            localStorage.setItem('books', JSON.stringify(books))
+}
+
+
 
 
